66e85dafb88b354b50649bf48c6d25accea0d3c4
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-20 13:20:04 +0500

    updated  files

--- app/src/main/java/com/example/bscssectionabatch20/Listviewexamples/ImageandTextAdapter.java
+++ app/src/main/java/com/example/bscssectionabatch20/Listviewexamples/ImageandTextAdapter.java
@@ -2,9 +2,17 @@ package com.example.bscssectionabatch20.Listviewexamples;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bscssectionabatch20.R;
 
 public class ImageandTextAdapter extends ArrayAdapter<String> {
     Activity activity;
@@ -12,13 +20,31 @@ public class ImageandTextAdapter extends ArrayAdapter<String> {
     String [] Text;
      int [] Image;
 
-    public ImageandTextAdapter(@NonNull Context context, int resource, Activity activity, String[] name, String[] text, int[] image) {
-        super(context, resource);
+    public ImageandTextAdapter(@NonNull  int resource, Activity activity, String[] name, String[] text, int[] image) {
+        super(activity, R.layout.imageandtext,text);
         this.activity = activity;
         Name = name;
         Text = text;
         Image = image;
+
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View View, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View ViewHolder = inflater.inflate(R.layout.imageandtext,null);
+        TextView txtName = ViewHolder.findViewById(R.id.txtoneimgandtxt);
+        TextView txtText = ViewHolder.findViewById(R.id.txttwoimgandtxt);
+        ImageView imageView = ViewHolder.findViewById(R.id.imgimgandtxt);
+        txtName.setText(Name[position]);
+        txtText.setText(Text[position]);
+        imageView.setImageResource(Image[position]);
+        return ViewHolder;
 
+
+
+
+
+    }
 }
