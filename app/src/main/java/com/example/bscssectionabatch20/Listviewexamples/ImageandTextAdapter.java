c298163fc9aa91fdf7170be625d10b63e6e92204
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-20 13:00:23 +0500

    Update ImageandTextAdapter.java

--- app/src/main/java/com/example/bscssectionabatch20/Listviewexamples/ImageandTextAdapter.java
+++ app/src/main/java/com/example/bscssectionabatch20/Listviewexamples/ImageandTextAdapter.java
@@ -1,5 +1,24 @@
 package com.example.bscssectionabatch20.Listviewexamples;
 
-public class ImageandTextAdapter {
-    
+import android.app.Activity;
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+public class ImageandTextAdapter extends ArrayAdapter<String> {
+    Activity activity;
+    String [] Name;
+    String [] Text;
+     int [] Image;
+
+    public ImageandTextAdapter(@NonNull Context context, int resource, Activity activity, String[] name, String[] text, int[] image) {
+        super(context, resource);
+        this.activity = activity;
+        Name = name;
+        Text = text;
+        Image = image;
+    }
+
+
 }
