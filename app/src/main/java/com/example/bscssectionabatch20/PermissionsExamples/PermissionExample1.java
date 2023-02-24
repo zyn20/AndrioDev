23857c3299883a51776908c4590ec33016fb8d9e
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-24 11:00:52 +0500

    Update PermissionExample1.java

--- app/src/main/java/com/example/bscssectionabatch20/PermissionsExamples/PermissionExample1.java
+++ app/src/main/java/com/example/bscssectionabatch20/PermissionsExamples/PermissionExample1.java
@@ -6,6 +6,7 @@ import static android.Manifest.permission.CAMERA;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.View;
@@ -24,10 +25,11 @@ public class PermissionExample1 extends AppCompatActivity {
 
     }
 
-    public void CheckPermission(View view) {
+    public boolean CheckPermission(View view) {
         int result_P1 = ContextCompat.checkSelfPermission(this,ACCESS_FINE_LOCATION);
         int result_P2 = ContextCompat.checkSelfPermission(this, CAMERA);
-        
+        return result_P1 == PackageManager.PERMISSION_GRANTED &&
+                result_P2 == PackageManager.PERMISSION_GRANTED;
 
 
     }
