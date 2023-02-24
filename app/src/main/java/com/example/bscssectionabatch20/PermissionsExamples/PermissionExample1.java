08ea0adbd4ddfca328988b113eaaf840a2694c4e
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-02-24 10:51:10 +0500

    Permission Example 1

--- app/src/main/java/com/example/bscssectionabatch20/PermissionsExamples/PermissionExample1.java
+++ app/src/main/java/com/example/bscssectionabatch20/PermissionsExamples/PermissionExample1.java
@@ -3,6 +3,7 @@ package com.example.bscssectionabatch20.PermissionsExamples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bscssectionabatch20.R;
 
@@ -12,5 +13,13 @@ public class PermissionExample1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission_example1);
+    }
+
+    public void RequestForPermission(View view) {
+
+    }
+
+    public void CheckPermission(View view) {
+        
     }
 }
