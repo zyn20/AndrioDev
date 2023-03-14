bc1d49d42c16a108edfcea6bbb619816f22c28dc
Zain Ul Abideen <mzainrajput6868@gmail.com>
2023-03-14 12:49:39 +0500

    bound Service

--- app/src/main/java/com/example/bscssectionabatch20/serviceExample/BoundServiceExample/BoundServiceExampleMainActivity.java
+++ app/src/main/java/com/example/bscssectionabatch20/serviceExample/BoundServiceExample/BoundServiceExampleMainActivity.java
@@ -2,11 +2,35 @@ package com.example.bscssectionabatch20.serviceExample.BoundServiceExample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
 
 import com.example.bscssectionabatch20.R;
 
 public class BoundServiceExampleMainActivity extends AppCompatActivity {
+
+    public MyBoundService myBoundServicePlayer;
+    public boolean myBoundServiceFlag = false;
+    private ServiceConnection myServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder iBinder) {
+            MyBoundService.MyBinder myBinder = (MyBoundService.MyBinder) iBinder;
+            myBoundServicePlayer = myBinder.getServiceMethod();
+            myBoundServiceFlag = true;
+            Log.d("Tag","Connection Created");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +39,34 @@ public class BoundServiceExampleMainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this,MyBoundService.class);
+        bindService(intent,myServiceConnection,BIND_AUTO_CREATE);
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(myBoundServiceFlag==true)
+        {
+            unbindService(myServiceConnection);
+
+        }
+    }
+
+    public void Play_Pause(View view) {
+        if(myBoundServiceFlag == true)
+        {
+            if(myBoundServicePlayer.isPlaying())
+            {
+                myBoundServicePlayer.Pause();
+            }
+            else myBoundServicePlayer.Play();
+        }
+
+    }
 }
--- app/src/main/java/com/example/bscssectionabatch20/serviceExample/BoundServiceExample/MyBoundService.java
+++ app/src/main/java/com/example/bscssectionabatch20/serviceExample/BoundServiceExample/MyBoundService.java
@@ -2,15 +2,70 @@ package com.example.bscssectionabatch20.serviceExample.BoundServiceExample;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.example.bscssectionabatch20.R;
+
 public class MyBoundService extends Service {
+    private final Binder myBinder = new MyBinder();
+
+    public static final String TAG = "TAG";
+    public MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.small);
+        Log.d(TAG,"Oncreate Method is Called__________");
+    }
 
     @Nullable
     @Override
+
     public IBinder onBind(Intent intent) {
-        return null;
+
+        Log.d(TAG,"onBind called _______");
+        return myBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+
+    //When bound service then binder msut be extend
+    public class MyBinder extends Binder
+    {
+        MyBoundService getServiceMethod()
+        {
+            //return the context of its parent class
+            return MyBoundService.this;
+        }
+
+
+    }
+    public void Play()
+    {
+        mediaPlayer.start();
+    }
+    public void Pause()
+    {
+        mediaPlayer.stop();
+    }
+    public boolean isPlaying()
+    {
+       return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
     }
 }
